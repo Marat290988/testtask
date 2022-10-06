e479aeb0703fec6ed900a6f274a5dc6682177665
Marat <marat.zinatulin@fido.uz>
2022-10-06 14:48:39 +0500

    work with sort in grid

--- src/app/components/paginator/paginator.component.ts
+++ src/app/components/paginator/paginator.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-paginator',
@@ -8,7 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class PaginatorComponent implements OnInit {
 
   @Input() paginationData!: any[];
+  @Output() changePageEmit = new EventEmitter();
   currentPage = 1;
+  beginPage = 1;
   maxPage!: number;
   pageElems: any[] = [];
 
@@ -18,34 +20,94 @@ export class PaginatorComponent implements OnInit {
     this.maxPage = this.paginationData.length / 5;
     this.pageElems = [
       {title: '...', state: false, selected: false},
-      {title: this.currentPage, state: true, selected: true},
-      {title: this.currentPage+1, state: true, selected: false},
-      {title: this.currentPage+2, state: true, selected: false},
+      {title: this.beginPage, state: true, selected: true},
+      {title: this.beginPage+1, state: true, selected: false},
+      {title: this.beginPage+2, state: true, selected: false},
       {title: '...', state: true, selected: false},
       {title: this.maxPage, state: true, selected: false}
     ]
   }
 
-  onChangePage(num: any) {
-    if (num === '...') {
+  onChangePage(num: any, index: any, direction: string) {
+    console.log(this.currentPage)
+    console.log(this.maxPage)
+    if (direction === 'NEXT' && this.currentPage < this.maxPage-1) {
+      this.currentPage++;
+      this.changePageEmit.emit(this.currentPage);
+      if (this.currentPage - this.beginPage === 1) {
+        this.pageSelected(2);
+      } else if (this.currentPage - this.beginPage === 2 && this.maxPage - this.currentPage !== 2) {
+        this.pageSelected(3);
+      } 
+      else if (this.currentPage - this.beginPage >= 2) {
+        this.beginPage++;
+        this.pageSelected(3);
+        this.setPagElems();
+      }
+      return;
+    } else if (direction === 'NEXT' && this.currentPage < this.maxPage) {
+      this.currentPage++;
+      this.changePageEmit.emit(this.currentPage);
+      this.pageSelected(5);
       return;
     }
-    this.currentPage = num;
+    if (direction === 'PREV' && this.currentPage > 1) {
+      this.currentPage--;
+      this.changePageEmit.emit(this.currentPage);
+      if (this.currentPage - this.beginPage === 1) {
+        this.pageSelected(2);
+      } else if (this.currentPage - this.beginPage === 2) {
+        this.pageSelected(3);
+      } else if (this.currentPage - this.beginPage === 0) {
+        this.pageSelected(1);
+      } else if (this.currentPage < this.beginPage) {
+        this.beginPage--;
+        this.pageSelected(1);
+        this.setPagElems();
+        return;
+      }
+    }
+    if (direction === 'CLICK') {
+      this.pageSelected(index);
+      this.currentPage = +num;
+      this.changePageEmit.emit(this.currentPage);
+      return;
+    }
+    if (direction === 'MAX') {
+      this.currentPage  = +num;
+      this.changePageEmit.emit(this.currentPage);
+      this.beginPage = this.maxPage-3;
+      this.pageSelected(5);
+      this.setPagElems();
+      return;
+    }
+  }
+
+  pageSelected(index: number) {
+    this.pageElems[1].selected = false;
+    this.pageElems[2].selected = false;
+    this.pageElems[3].selected = false;
+    this.pageElems[5].selected = false;
+    this.pageElems[index].selected = true;
   }
 
   setPagElems() {
-    if (this.currentPage>3) {
+    if (this.beginPage > 1) {
       this.pageElems[0].state = true;
     } else {
       this.pageElems[0].state = false;
     }
-    if (this.paginationData.length > 1) {
+    if (this.maxPage > 1) {
+      this.pageElems[2].state = true;
+    } else {
       this.pageElems[2].state = false;
     }
-    if (this.paginationData.length > 2) {
+    if (this.maxPage > 2) {
+      this.pageElems[3].state = true;
+    } else {
       this.pageElems[3].state = false;
     }
-    if (this.currentPage === this.paginationData.length-1) {
+    if (this.beginPage > this.maxPage - 4) {
       this.pageElems[4].state = false;
     } else {
       this.pageElems[4].state = true;
--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -16,9 +16,13 @@ export class PostsComponent implements OnInit {
 
   viewPosts: Post[] = [];
   loader = false;
-  titleTable: string[] = [
-    'ID', 'User', 'Title', 'Content'
+  titleTable: any[] = [
+    {title: 'ID', state: false, type: 'NUM', key: 'id'}, 
+    {title: 'User', state: false, type: 'NUM', key: 'userId'}, 
+    {title: 'Title', state: false, type: 'STR', key: 'title'}, 
+    {title: 'Content', state: false, type: 'STR', key: 'body'}
   ];
+  allPosts: any[] = [];
 
   constructor(
     public storage: StorageService
@@ -30,18 +34,96 @@ export class PostsComponent implements OnInit {
       const subs = this.storage.getAllData()
         .subscribe(()=> {
           this.loader = false;
+          this.allPosts = [...this.storage.posts];
           this.setViewPosts(1);
           subs.unsubscribe();
         })
     } else {
+      this.allPosts = [...this.storage.posts];
       this.setViewPosts(1);
     }
-    console.log(this.storage.posts)
+    console.log(this.allPosts)
+  }
+
+  sortPosts(key: string, stateDirection: boolean, type: string) {
+    if (stateDirection && type === 'STR') {
+      this.allPosts.sort((a, b) => {
+        const stringA = a[key].toLowerCase();
+        const stringB = b[key].toLowerCase();
+        if (stringA < stringB) {
+          return -1;
+        }
+        if (stringA > stringB) {
+          return 1;
+        }
+        return 0;
+      })
+    } else if (type === 'STR') {
+      this.allPosts.sort((a, b) => {
+        const stringA = a[key].toLowerCase();
+        const stringB = b[key].toLowerCase();
+        if (stringA < stringB) {
+          return 1;
+        }
+        if (stringA > stringB) {
+          return -1;
+        }
+        return 0;
+      })
+    }
+    if (stateDirection && type === 'NUM') {
+      this.allPosts.sort((a, b) => {
+        const stringA = a[key];
+        const stringB = b[key];
+        if (stringA < stringB) {
+          return -1;
+        }
+        if (stringA > stringB) {
+          return 1;
+        }
+        return 0;
+      })
+    } else if (type === 'NUM') {
+      this.allPosts.sort((a, b) => {
+        const stringA = a[key];
+        const stringB = b[key];
+        if (stringA < stringB) {
+          return 1;
+        }
+        if (stringA > stringB) {
+          return -1;
+        }
+        return 0;
+      })
+    }
+    this.setViewPosts(1);
+  }
+
+  onClickSort(head: {state: boolean, type: string, key: string}, par: HTMLDivElement) {
+    const state = head.state;
+    document.querySelectorAll('.d-flex.active').forEach(el => {
+      el.classList.remove('active');
+    })
+    par.classList.add('active');
+    for (let key in this.titleTable) {
+      this.titleTable[key].state = false;
+    }
+    if (state) {
+      head.state = !state;
+      this.sortPosts(head.key, true, head.type);
+    } else {
+      head.state = !state;
+      this.sortPosts(head.key, false, head.type);
+    }
   }
 
   setViewPosts(page: number): void {
     const index = page * 5;
-    this.viewPosts = this.storage.posts.slice(index-5, index);
+    this.viewPosts = this.allPosts.slice(index-5, index);
+  }
+
+  onChangePage(page: number) {
+    this.setViewPosts(page);
   }
 
 }
